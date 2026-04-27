2a8e78e8984ee1edea19819991df3608f906ddf5
goldkorea974-blip <goldkorea974@gmail.com>
2026-04-27 13:18:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "PUT_YOUR_TOKEN_HERE"
+TOKEN = "8165343576:AAGr_uWTBUMGCgcdahiCicHN3DehLaBOUf0"
 CHAT_ID = 940590742
 
 last_prices = ""
